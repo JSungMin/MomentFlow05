ecb2b5e5e031065c0435673f440525e5aa8ffeaf
= <jsm1505104@gmail.com>
2017-03-01 12:34:32 +0900

    Scene01~02

--- Assets/Scripts/Camera/FollowUpCamera.cs
+++ Assets/Scripts/Camera/FollowUpCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowUpCamera : MonoBehaviour {
 
 	public Transform followTarget;
-
+	public LayerMask mask;
 	private Camera mainCamera;
 
 	public float followSpeed = 2;
@@ -16,14 +16,52 @@ public class FollowUpCamera : MonoBehaviour {
 
 	public bool isActive = true;
 
+	Vector2 input;
+
 	// Use this for initialization
 	void Start () {
 		mainCamera = GetComponent<Camera> ();	
 	}
-	
+
+	bool isToLeft = false;
+	bool isToRight = false;
+
+	public void RaycastHorizontal(){
+		var max = followTarget.GetComponent<BoxCollider2D> ().bounds.max;
+		var min = followTarget.GetComponent<BoxCollider2D> ().bounds.min;
+
+		var len = max.y - min.y;
+
+		//if (followTarget.GetComponent<Player> ().input.x != 0)
+		//	input = followTarget.GetComponent<Player> ().input;
+
+			for (int i = 0; i < 3; i++) {
+				RaycastHit2D hit = Physics2D.Raycast (new Vector2 (max.x, min.y + len * ((float)i / (float)2)), Vector2.right, 1.6f,mask);
+				Debug.DrawLine (new Vector2 (max.x, min.y + len * ((float)i / (float)2)), new Vector2 (max.x, min.y + len * ((float)i / (float)2)) + Vector2.right*2);
+				//Debug.DrawLine (new Vector2 (max.x, min.y + len * ((float)i / (float)2)), new Vector2 (max.x, min.y + len * ((float)i / (float)2)) + Info.moveSpeed * Vector2.right * Time.deltaTime,Color.red);
+				if (hit.collider != null) {
+					
+				isToRight = true;
+				} else {
+				isToRight = false;
+				}
+			}
+			for (int i = 0; i < 3; i++) {
+				RaycastHit2D hit = Physics2D.Raycast (new Vector2 (min.x, min.y + len * ((float)i / (float)2)), Vector2.left, 1.6f,mask);
+				Debug.DrawLine (new Vector2 (min.x, min.y + len * ((float)i / (float)2)), new Vector2 (max.x, min.y + len * ((float)i / (float)2)) + Vector2.left*2);
+				//Debug.DrawLine (new Vector2 (min.x, min.y + len * ((float)i / (float)2)),new Vector2 (min.x, min.y + len * ((float)i / (float)2)) + Info.moveSpeed * Vector2.left * Time.deltaTime,Color.red);
+				if (hit.collider != null) {
+				isToLeft = true;
+				} else {
+				isToLeft = false;
+				}
+			}
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (isActive) {
+		RaycastHorizontal ();
+		if (!isToLeft&&!isToRight) {
 			var targetPos = followTarget.position;
 			targetPos.z = 0;
 			var nowPos = mainCamera.transform.position;
@@ -33,6 +71,8 @@ public class FollowUpCamera : MonoBehaviour {
 			var pos = Vector3.Lerp (nowPos, targetPos + tmpVector, followSpeed * Time.deltaTime);
 			pos.z = -30;
 			mainCamera.transform.position = pos;
+		} else {
+
 		}
 	}
 }
--- Assets/Scripts/InteractObject/BusStop.cs
+++ Assets/Scripts/InteractObject/BusStop.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BusStop : InteractObject {
 	public CutSceneUnit bus;
 
+	public TweenAlpha fadeOutCam;
+
 	public void CallBus(){
 		if (!isInteract) {
 			Debug.Log ("Talk");
-			bus.isAction = true;
+			fadeOutCam.PlayForward ();
 			isInteract = true;
 		}
 	}
 	public void DropBus(){
 		isInteract = false;
 		GameObject.FindObjectOfType<InteractiveManager> ().nowInteract = false;
+	}
+
+	public void NextScene(){
+		SceneManager.LoadScene (1);	
 	}
 
 	// Use this for initialization
