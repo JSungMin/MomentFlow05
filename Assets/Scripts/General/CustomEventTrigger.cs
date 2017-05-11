79d20470eb43f9d4ff78cc68bc635f9190f8f87a
park <wlgur01014@naver.com>
2017-05-12 05:20:07 +0900

    factory

--- Assets/Scripts/General/CustomEventTrigger.cs
+++ Assets/Scripts/General/CustomEventTrigger.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class CustomEventTrigger : MonoBehaviour {
-	public UnityEvent triggerEvent;
+public class CustomEventTrigger : MonoBehaviour
+{
+    private Player player;
+    public UnityEvent triggerEvent;
+    public FrontWall frontWall;
 
-	public void StartTriggerEvent(){
-		triggerEvent.Invoke ();
-	}
+    private void Awake()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+    }
+
+    public void StartTriggerEvent()
+    {
+        triggerEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            if (other.transform.position.x > transform.position.x)
+                frontWall.TransparentSlowly();
+            else
+                frontWall.UnTransparentSlowly();
+        }
+    }
 }
---
+++ Assets/Scripts/General/FrontWall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontWall : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private const float deltaTm = 0.015f;
+    private float nowTm = 0.0f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void TransparentSlowly()
+    {
+        StartCoroutine(TransparentSlowlyCo(0.3f));
+    }
+
+    public void UnTransparentSlowly()
+    {
+        StartCoroutine(UnTransparentSlowlyCo(0.3f));
+    }
+
+    private IEnumerator TransparentSlowlyCo(float excuteTm)
+    {
+        nowTm = 0.0f;
+        while (nowTm < excuteTm)
+        {
+            spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 1.0f - nowTm / excuteTm);
+            nowTm += deltaTm;
+            yield return new WaitForSeconds(deltaTm);
+        }
+        spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+    }
+
+    private IEnumerator UnTransparentSlowlyCo(float excuteTm)
+    {
+        nowTm = 0.0f;
+        while (nowTm < excuteTm)
+        {
+            spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, nowTm / excuteTm);
+
+            nowTm += deltaTm;
+            yield return new WaitForSeconds(deltaTm);
+        }
+        spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+}
